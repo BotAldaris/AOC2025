5e622078aea5c09dcb2bf2901fdeb42dbb925463
BotAldaris <biells2002@gmail.com>
2025-12-23 09:53:38 -0300

    Finalizing day01

--- src/day01/part1.c
+++ src/day01/part1.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_number(char buffer[1024]);
 int main() {
   FILE *file;
-  fopen_s(&file,
-          "C:\\Users\\biel_\\Documents\\projetos\\AOC2025\\src\\day1\\.txt",
-          "r");
+  fopen_s(&file, "./src/day01/input.txt", "r");
   char buffer[1024]; // Buffer for reading lines
   int pivot = 50;
   int count = 0;
-  int before;
   while (fgets(buffer, sizeof(buffer), file) != NULL) {
-    before = pivot;
     if (buffer[0] == 'L') {
       pivot -= atoi(buffer + 1);
-    } else {
+    } else if (buffer[0] == 'R') {
       pivot += atoi(buffer + 1);
     }
-
     if (pivot > 99) {
-      count += pivot / 100;
       pivot = pivot % 100;
-    } else if (pivot <= 0) {
-      count++;
-      if (before == 0) {
-        count--;
+    } else if (pivot < 0) {
+      pivot = pivot % 100;
+      if (pivot != 0) {
+        pivot += 100;
       }
-      count += abs(pivot / 100);
-      pivot = 100 + pivot % 100;
+    }
+    if (pivot == 0) {
+      count += 1;
     }
   }
+  fclose(file);
   printf("%d\n", count);
 }
--- src/day01/part2.c
+++ src/day01/part2.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_number(char buffer[1024]);
 int main() {
   FILE *file;
   fopen_s(
       &file,
-      "C:\\Users\\biel_\\Documents\\projetos\\AOC2025\\src\\day1\\input.txt",
+      "C:\\Users\\biel_\\Documents\\projetos\\AOC2025\\src\\day01\\input.txt",
       "r");
   char buffer[1024]; // Buffer for reading lines
   int pivot = 50;
   int count = 0;
+  int before;
   while (fgets(buffer, sizeof(buffer), file) != NULL) {
     if (buffer[0] == 'L') {
-      pivot -= atoi(buffer + 1);
-    } else if (buffer[0] == 'R') {
-      pivot += atoi(buffer + 1);
-    }
-    if (pivot > 99) {
-      pivot = pivot % 100;
-    } else if (pivot < 0) {
-      pivot = pivot % 100;
-      if (pivot != 0) {
-        pivot += 100;
+      if (pivot == 0) {
+        count--;
       }
-    }
-    if (pivot == 0) {
-      count += 1;
+      pivot -= atoi(buffer + 1);
+      if (pivot <= 0) {
+        count += abs(pivot / 100) + 1;
+        pivot = ((pivot % 100 + 100) % 100);
+      }
+    } else {
+      pivot += atoi(buffer + 1);
+      count += pivot / 100;
+      pivot = pivot % 100;
     }
   }
-  fclose(file);
   printf("%d\n", count);
 }
